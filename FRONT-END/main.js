aec1167fb40209b86561dbc6ef85343a24a19092
Andsirpa <andsirpa94@gmail.com>
2024-03-02 12:18:03 +0100

    add: created taskList in main.js

--- FRONT-END/main.js
+++ FRONT-END/main.js
@@ -3,7 +3,24 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-            title: 'Todo List'
+            title: 'Todo List',
+
+            // array di oggetti
+            taskList: [
+                // do a ciascun oggetto un testo e uno status 
+                {
+                    text: 'Task 1',
+                    done: true,
+                },
+                {
+                    text: 'Task 2',
+                    done: false,
+                },
+                {
+                    text: 'Task 3',
+                    done: false,
+                },
+            ]
         };
     },
 });
