54e0486e789f0fcfc68227af5944c28ed4654009
Andsirpa <andsirpa94@gmail.com>
2024-03-03 14:46:00 +0100

    add: get tasklist in php from taskList.json

---
+++ BACK-END/api/get-tasklist.php
@@ -0,0 +1,8 @@
+<?php
+// entro nel file tasklist
+$tasklist_json = file_get_contents('../taskList.json');
+
+// specifico il tipo di file (json)
+header('Content-Type: application/json; charset=utf-8');
+
+echo $tasklist_json;
--- FRONT-END/main.js
+++ FRONT-END/main.js
@@ -8,21 +8,25 @@ const app = createApp({
             // array di oggetti
             taskList: [
                 // do a ciascun oggetto un testo e uno status 
-                {
-                    text: 'Task 1',
-                    done: true,
-                },
-                {
-                    text: 'Task 2',
-                    done: false,
-                },
-                {
-                    text: 'Task 3',
-                    done: false,
-                },
-            ]
+                // {
+                //     text: 'Task 1',
+                //     done: true,
+                // },
+                // {
+                //     text: 'Task 2',
+                //     done: false,
+                // },
+                // {
+                //     text: 'Task 3',
+                //     done: false,
+                // },
+            ],
         };
     },
+
+    methods: {
+        // chiamo la taskList dalla API
+    }
 });
 
 app.mount('#app');
